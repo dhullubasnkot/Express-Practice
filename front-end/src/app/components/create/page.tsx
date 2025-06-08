6d8fdfef1f819d780398ab8ccf0ee358d3a7dc61
dhullubasnkot <123baglung@gmail.com>
2025-06-08 14:54:09 +0545

    updated

--- front-end/src/app/components/create/page.tsx
+++ front-end/src/app/components/create/page.tsx
@@ -39,7 +39,7 @@ export default function CreateProduct() {
       alert(`Error: ${error.message}`);
     }
   };
-
+// hello
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="bg-white p-8 rounded-lg shadow-xl max-w-md w-full border border-gray-200">
